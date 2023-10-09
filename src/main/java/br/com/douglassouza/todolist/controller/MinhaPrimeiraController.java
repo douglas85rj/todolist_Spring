fd1de21fb83262e2211850815bdc65162b2bdb8e
douglas85rj <douglas85rj@gmail.com>
2023-10-09 12:39:11 -0300

    Controller

---
+++ src/main/java/br/com/douglassouza/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,13 @@
+package br.com.douglassouza.todolist.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+ @Controller
+ @RestController
+ 
+
+public class MinhaPrimeiraController {
+   
+}
