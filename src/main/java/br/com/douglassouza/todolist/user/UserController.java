b56049e60d97d4caf7eb39b0bac2e71d6cd9dfb2
douglas85rj <douglas85rj@gmail.com>
2023-10-11 14:31:28 -0300

    post_method

--- src/main/java/br/com/douglassouza/todolist/user/UserController.java
+++ src/main/java/br/com/douglassouza/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.douglassouza.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-public String user() {
-return "user";
-
-    }
+@PostMapping("/")
+public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.name);
+}
 
 }
