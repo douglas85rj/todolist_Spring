d4ab575caec67ea1786a822a0751bbbf44146f1c
douglas85rj <douglas85rj@gmail.com>
2023-10-13 01:05:37 -0300

    JPA_H2_Persist

--- src/main/java/br/com/douglassouza/todolist/user/UserModel.java
+++ src/main/java/br/com/douglassouza/todolist/user/UserModel.java
@@ -1,10 +1,29 @@
 package br.com.douglassouza.todolist.user;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_users")
 public class UserModel {
-    
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDate createdAt; 
     
     public void setUsername(String username) {
         this.username = username;
@@ -29,6 +48,6 @@ public class UserModel {
     public String getPassword() {
         return password;
     }
-    
+
 
 }
