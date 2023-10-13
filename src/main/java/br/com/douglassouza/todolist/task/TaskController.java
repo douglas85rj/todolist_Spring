dae2cff6fc969cb1ca1d394a2d2c8d0555a101e4
douglas85rj <douglas85rj@gmail.com>
2023-10-13 15:18:33 -0300

    Method_update

--- src/main/java/br/com/douglassouza/todolist/task/TaskController.java
+++ src/main/java/br/com/douglassouza/todolist/task/TaskController.java
@@ -7,11 +7,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.douglassouza.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -45,6 +48,16 @@ public class TaskController {
         var idUser = (UUID) request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser(idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNomNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
+
     }
 
 }
